dc489ba1134e62c1808f4889d3a2e11be5f81d4d
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2020-12-30 17:40:45 +0530

    adding.uc3

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,28 @@
 #!/bin/bash -x
-for (( i=0; i<=100; i++))
+heads=0
+tails=0
+maxflips=21
+while (( $heads<$maxflips && $tails<$maxflips ))
 do
 	simulator=$((RANDOM%2))
-	if [ $simulator -eq 1 ]
+	if (( $simulator == 1 ))
 	then
 		heads=$((heads+1))
-	else
+	else (( $simulator == 0 ))
 		tails=$((tails+1))
 	fi
 done
-echo "HEADS WON" $heads "TIMES"
-echo "TAILS WON" $tails "TIMES"
+
+if (( $heads == $tails ))
+then
+	echo "tie"
+else
+	if (( $heads > $tails ))
+	then
+	diff=$(($heads-$tails))
+	echo "HEADS HAS WON BY" $diff
+	else (( $tails > $heads ))
+	diff=$(($tails-$heads))
+	echo "TAILS HAS WON BY" $diff
+fi
+	fi
