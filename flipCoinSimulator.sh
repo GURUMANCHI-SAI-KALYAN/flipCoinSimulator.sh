50edcb7b891eac5bc7fb5faefa783820e4211cf6
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2020-12-30 16:56:49 +0530

    adding. uc2 file

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
-Heads=1
-Tails=0
-simulator=$((RANDOM%2))
-if [ $simulator -eq $Heads ]
-then
-	echo "winner"
-else
-	echo "looser"
-fi
+for (( i=0; i<=100; i++))
+do
+	simulator=$((RANDOM%2))
+	if [ $simulator -eq 1 ]
+	then
+		heads=$((heads+1))
+	else
+		tails=$((tails+1))
+	fi
+done
+echo "HEADS WON" $heads "TIMES"
+echo "TAILS WON" $tails "TIMES"
